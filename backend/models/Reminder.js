3cc2827a8f22d4e0115dafa11b7bac03eb2f407a
Parth Kalani <121436032+parth1899@users.noreply.github.com>
2024-03-12 11:24:47 +0530

    Update Reminder.js

--- backend/models/Reminder.js
+++ backend/models/Reminder.js
@@ -1,1 +1,27 @@
+const mongoose = require("mongoose");
 
+const reminderSchema = new mongoose.Schema({
+
+  user:{
+   type : mongoose.Schema.Types.ObjectId,
+   ref : 'user'
+  },
+  reminderMsg: {
+    type: String,
+    required: true,
+  },
+  remindAt: {
+    type: String,
+    required: true,
+  },
+  reminderType:{
+   
+    type : String,
+  },
+  isReminded: {
+    type: Boolean,
+  },
+});
+// const Reminder = new mongoose.model("reminder", reminderSchema);
+
+module.exports=mongoose.model("reminder", reminderSchema);
